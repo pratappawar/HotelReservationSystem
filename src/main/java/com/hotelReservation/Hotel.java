57f66f15f1d34bb982d89431c938b288cf220873
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-21 14:35:23 +0530

    Added: Welcome to Hotel Reservation System program

---
+++ src/main/java/com/hotelReservation/Hotel.java
@@ -0,0 +1,5 @@
+package com.hotelReservation;
+
+public class Hotel {
+
+}
---
+++ src/main/java/com/hotelReservation/HotelReservationService.java
@@ -0,0 +1,7 @@
+package com.hotelReservation;
+
+public class HotelReservationService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
