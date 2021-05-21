d4048965a7797ce7a4aa9272d5de060e2126e6a9
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-21 14:58:57 +0530

    Added: Add hotel in HotelReservationSystem with name and rate

--- src/main/java/com/hotelReservation/Hotel.java
+++ src/main/java/com/hotelReservation/Hotel.java
@@ -1,5 +1,42 @@
 package com.hotelReservation;
 
 public class Hotel {
+    /**
+     * properties of Hotel class
+     */
+    private String hotelName;
+    private double dailyRate;
 
+    /**
+     * Parameterized constructor
+     * @param hotelName
+     * @param dailyRate
+     */
+    public Hotel(String hotelName, double dailyRate) {
+        this.hotelName = hotelName;
+        this.dailyRate = dailyRate;
+    }
+
+    /**
+     * Setters
+     * @return
+     */
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    /**
+     * @return daily customer rate
+     */
+    public double getDailyRate() {
+        return dailyRate;
+    }
+
+    /**
+     * set daily rate
+     * @param
+     */
+    public void setDailyRate(double dailyRate) {
+        this.dailyRate = dailyRate;
+    }
 }
--- src/main/java/com/hotelReservation/HotelReservationService.java
+++ src/main/java/com/hotelReservation/HotelReservationService.java
@@ -1,6 +1,28 @@
 package com.hotelReservation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotelReservationService {
+
+    List<Hotel> hotels=new ArrayList<Hotel>();
+
+    /**
+     * Add hotel method to add hotel in list
+     * @param hotel
+     */
+    public void addHotel(Hotel hotel){
+        this.hotels.add(hotel);
+    }
+
+    /**
+     * It will give hotel list
+     * @return
+     */
+    public List<Hotel> getHotels(){
+        return hotels;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
     }
